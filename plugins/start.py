49d94709282c93dd6e8a962a71bd5bf1e16f20a8
81jokeryt-svg <81jokeryt@gmail.com>
2026-05-21 15:07:30 +0530

    Import verification module in start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 import humanize
 from config import MSG_EFFECT, OWNER_ID
-from plugins.shortner import get_short
+from plugins.verification import *
 from helper.helper_func import get_messages, force_sub, decode, batch_auto_del_notification
 import asyncio
 
